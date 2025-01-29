f0e722f78f1e95740c197c21c3c3f1e13ddcb326
Damir Perkosan <dperkosan@gmail.com>
2025-01-29 10:26:41 +0100

    remove log in tests

--- src/common/log/app.log.ts
+++ src/common/log/app.log.ts
@@ -1,8 +1,14 @@
 import { createLogger, format, transports } from 'winston';
 import getEnvVariable from '@common/utils/env.util';
 
+const isTestEnv = getEnvVariable('NODE_ENV') === 'test';
+
 const logger = createLogger({
-  level: getEnvVariable('NODE_ENV') === 'development' ? 'debug' : 'info',
+  level: isTestEnv
+    ? 'silent' // Suppress logs in test environment
+    : getEnvVariable('NODE_ENV') === 'development'
+      ? 'debug'
+      : 'info',
   format: format.combine(
     format.timestamp(),
     format.errors({ stack: true }), // Include stack traces for errors
@@ -10,14 +16,23 @@ const logger = createLogger({
   ),
   transports: [
     new transports.Console({
+      silent: isTestEnv, // Disable console logs in test environment
       format: format.combine(
         format.colorize(), // Add colors to console logs
         format.simple(), // Simplify console log format
       ),
     }),
-    new transports.File({ filename: 'logs/error.log', level: 'error' }), // Write errors to a file
-    new transports.File({ filename: 'logs/combined.log' }), // Write all logs to a file
-  ],
+    !isTestEnv
+      ? new transports.File({ filename: 'logs/error.log', level: 'error' })
+      : null, // Write errors to a file
+    !isTestEnv ? new transports.File({ filename: 'logs/combined.log' }) : null, // Write all logs to a file
+  ].filter(
+    (
+      transport,
+    ): transport is
+      | transports.ConsoleTransportInstance
+      | transports.FileTransportInstance => !!transport,
+  ), // Filter out null transports
 });
 
 // Log unhandled exceptions
